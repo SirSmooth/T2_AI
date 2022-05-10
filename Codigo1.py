88657b62dda8e269b71912d6c825ec12ec097fcf
SirSmooth <80971911+SirSmooth@users.noreply.github.com>
2022-05-09 21:37:45 -0400

    Update Codigo1.py
    p1 lista

--- Codigo1.py
+++ Codigo1.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sqlalchemy import true
 
 # 1)
 # a)
@@ -12,10 +13,9 @@ print(dfcrossmatches_small)
 
 classALeRCE = dfcrossmatches_small['classALeRCE']
 
-# print(classALeRCE)
-
+# graficamos
 pd.Index.value_counts(classALeRCE).plot.bar()
-plt.show()
+# plt.show()
 
 # b)
 # Leemos el archivo
@@ -23,3 +23,49 @@ features_small = pd.read_pickle("features_small.pickle")
 
 print(features_small)
 
+# pivoteamos
+fs_2 = features_small.pivot(columns = ['name', 'fid'], values = 'value')
+
+print(fs_2)
+
+# c)
+# vemos la media por columna
+fs_3 = fs_2.median()
+
+# reemplazamos los NaN
+fs_4 = fs_2.fillna(value=fs_3)
+
+print(fs_4)
+
+# d)
+# concatenamos
+conca = pd.concat([classALeRCE, fs_4], axis=1)
+
+print(conca)
+
+# eliminamos NaNs
+drop = conca.dropna()
+
+print(drop)
+
+# e)
+# graficamos
+pd.Index.value_counts(drop['classALeRCE']).plot.bar()
+# plt.show()
+
+# f)
+# agrupamos clases
+agru = drop.replace({'EA':'EB', 'EB/EW':'EB'})
+
+# se eliminan las clases ...
+elim = agru.loc[~agru['classALeRCE'].isin(['NLAGN', 'NLQSO', 'TDE', 'ZZ'])]
+print(elim)
+
+# g)
+# Matriz de diseno
+X = elim.drop(columns='classALeRCE')
+
+# Vector de nombres
+y = elim['classALeRCE']
+
+# 2
